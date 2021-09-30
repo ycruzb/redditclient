b211c105c38f591dcc8fd8d41216f4de19fff0b0
ycruzb <ycruzb@gmail.com>
2021-09-30 18:23:21 -0400

    fix: fix data fetching, improving where to fetch the data

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react"
+import { fetchTopPosts } from "../features/redditPosts/redditPostsSlice"
+import { useAppDispatch } from "../app/hooks"
 interface IProps {
 	children: React.ReactNode
 }
 
 const Layout = ({ children }: IProps) => {
+	const dispatch = useAppDispatch();
+
+	const handleGetPosts = async () => {
+		const resultAction = await dispatch(fetchTopPosts())
+		if (fetchTopPosts.fulfilled.match(resultAction)) {
+			/* const posts = resultAction.payload
+			console.log("POSTS: ", posts); */
+		} else {
+			console.log("Error fetching data!!!");
+		}
+	}
+
+	useEffect(() => {
+		handleGetPosts();
+	}, [])
+
 	return (
 		<>
 			{children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,12 @@
-import { useEffect } from "react"
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useAppSelector, useAppDispatch } from "../app/hooks"
-import { fetchTopPosts } from "../features/redditPosts/redditPostsSlice"
+import { useAppSelector } from "../app/hooks"
 import Sidebar from "../components/sidebar"
 import PostDetail from "../components/postDetail"
 import MobileMenu from "../components/mobileMenu"
 
 const Home: NextPage = () => {
 	const state = useAppSelector((state) => state.redditPosts)
-	const dispatch = useAppDispatch();
-
-	const handleGetPosts = async () => {
-		const resultAction = await dispatch(fetchTopPosts())
-		if (fetchTopPosts.fulfilled.match(resultAction)) {
-			/* const posts = resultAction.payload
-			console.log("POSTS: ", posts); */
-		} else {
-			console.log("Error fetching data!!!");
-		}
-	}
-
-	useEffect(() => {
-		handleGetPosts();
-	}, [])
 
 	return (
 		<>
